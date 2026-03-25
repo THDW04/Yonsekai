8494f09a4c4b6c8eb2dc8956fab23ff957f0370e
Haya-chb <haya.chaibi@hotmail.com>
2026-03-25 15:14:28 +0100

    add legal  terms

---
+++ frontend/src/pages/LegalTerms.jsx
@@ -0,0 +1,57 @@
+export const LegalTerms = () => {
+  return (
+    <main>
+      <h1>Mentions Légales</h1>
+
+      <section>
+        <h2>Editeur</h2>
+        <ul>
+          <li>Le site Yonsekai est édité par : Université Gustave Eiffel</li>
+          <li>Adresse : 5 Boulevard Descartes, 77420 Champs-sur-Marne, France</li>
+          <li>Contact : +33 01 60 95 75 00</li>
+          <li>SIRET : 130 026 123 00013</li>
+          <li>Code APE : 85.42Z</li>
+          <li>Forme juridique : EPSCP</li>
+          <li>Directeur de publication : Gilles Roussel</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Hébergement</h2>
+        <ul>
+          <li>Le site Yonsekai est hébergé par : O2switch</li>
+          <li>Adresse : Chem. des Pardiaux, 63000 Clermont-Ferrand, France</li>
+          <li>Numéro de téléphone : 04 44 44 60 40</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Politique de confidentialité</h2>
+        <p>
+          Le site collecte certaines données pour la création de comptes utilisateurs. 
+          Ces données sont traitées uniquement par Yonsekai et ne sont jamais vendues à des tiers. 
+          Conformément au RGPD, tout utilisateur peut accéder à ses données, demander leur modification 
+          ou leur suppression en contactant la DPO :
+        </p>
+        <ul>
+          <li>Nom : Véronique Juge</li>
+          <li>Adresse : 5 bd Descartes, 77420 Champs-sur-Marne</li>
+          <li>Contact : protectiondesdonnees-dpo@univ-eiffel.fr</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Membres du projet</h2>
+        <ul>
+          <li>Haya Chaibi - Développeuse</li>
+          <li>Nathalia Vilasse - Développeuse</li>
+          <li>Minh Anh Pham - </li>
+          <li>Sarah - </li>
+          <li>Sylia - </li>
+          <li>Moina - </li>
+          <li>Clément Kerneis - </li>
+        </ul>
+      </section>
+    </main>
+  );
+};
